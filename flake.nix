fee9cf4f4e9ae893c31717d4cb56117349f6bc31
lukasl-dev <git@lukasl.dev>
2026-06-18 22:51:19 +0200

    Package pi-codex-conversion

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "pi-codex-conversion";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      systems,
+      ...
+    }:
+    let
+      current = builtins.fromJSON (builtins.readFile ./version.json);
+      inherit (current)
+        version
+        rev
+        hash
+        npmDepsHash
+        ;
+
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+
+          monorepoSrc = pkgs.fetchFromGitHub {
+            owner = "IgorWarzocha";
+            repo = "howaboua-pi-stuff";
+            inherit rev hash;
+          };
+          src = "${monorepoSrc}/packages/pi-codex-conversion";
+        in
+        rec {
+          default = pi-codex-conversion;
+
+          pi-codex-conversion = pkgs.callPackage ./package.nix {
+            inherit src version npmDepsHash;
+          };
+
+          update-script-env = pkgs.symlinkJoin {
+            name = "pi-codex-conversion-update-script-env";
+            paths = [
+              pkgs.bash
+              pkgs.curl
+              pkgs.git
+              pkgs.jq
+              pkgs.nix
+              pkgs.nodejs
+              pkgs.prefetch-npm-deps
+            ];
+          };
+        }
+      );
+
+      formatter = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.nixfmt-rfc-style
+      );
+    };
+}
---
+++ package.nix
@@ -0,0 +1,122 @@
+{
+  lib,
+  stdenv,
+  buildNpmPackage,
+  rustPlatform,
+  pkg-config,
+  openssl,
+  nodejs,
+  src,
+  version,
+  npmDepsHash,
+}:
+
+let
+  platformArch =
+    {
+      x86_64-linux = "linux-x64";
+      aarch64-linux = "linux-arm64";
+      x86_64-darwin = "darwin-x64";
+      aarch64-darwin = "darwin-arm64";
+    }
+    .${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+
+  packageJson = removeAttrs (builtins.fromJSON (builtins.readFile "${src}/package.json")) [
+    "devDependencies"
+    "peerDependencies"
+  ];
+
+  tools = rustPlatform.buildRustPackage {
+    pname = "pi-codex-conversion-tools";
+    inherit version;
+
+    src = "${src}/src/tools";
+    cargoLock.lockFile = "${src}/src/tools/Cargo.lock";
+
+    nativeBuildInputs = [ pkg-config ];
+    buildInputs = [ openssl ];
+
+    doCheck = false;
+  };
+in
+buildNpmPackage {
+  pname = "pi-codex-conversion";
+  inherit version src npmDepsHash;
+
+  dontNpmBuild = true;
+  makeCacheWritable = true;
+  npmFlags = [
+    "--legacy-peer-deps"
+    "--omit=dev"
+  ];
+
+  postPatch = ''
+    cat > package.json <<'JSON'
+    ${builtins.toJSON packageJson}
+    JSON
+
+    cp ${./package-lock.json} package-lock.json
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out
+    cp -r \
+      src \
+      bin \
+      package.json \
+      tsconfig.json \
+      available-tools.png \
+      CHANGELOG.md \
+      PATH_TOOLS.md \
+      README.md \
+      LICENSE \
+      node_modules \
+      $out/
+
+    for script in apply_patch view_image web_run imagegen; do
+      substituteInPlace $out/bin/$script \
+        --replace-fail "#!/usr/bin/env node" "#!${nodejs}/bin/node"
+    done
+
+    rm -rf \
+      $out/src/tools/apply-patch/bin/* \
+      $out/src/tools/exec/bin/* \
+      $out/src/tools/view-image/bin/* \
+      $out/src/tools/web-run/bin/* \
+      $out/src/tools/imagegen/bin/*
+
+    mkdir -p \
+      $out/src/tools/apply-patch/bin/${platformArch} \
+      $out/src/tools/exec/bin/${platformArch} \
+      $out/src/tools/view-image/bin/${platformArch} \
+      $out/src/tools/web-run/bin/${platformArch} \
+      $out/src/tools/imagegen/bin/${platformArch}
+
+    cp ${tools}/bin/apply_patch \
+      $out/src/tools/apply-patch/bin/${platformArch}/apply_patch
+    cp ${tools}/bin/exec_bridge \
+      $out/src/tools/exec/bin/${platformArch}/exec_bridge
+    cp ${tools}/bin/view_image \
+      $out/src/tools/view-image/bin/${platformArch}/view_image
+    cp ${tools}/bin/web_run \
+      $out/src/tools/web-run/bin/${platformArch}/web_run
+    cp ${tools}/bin/imagegen \
+      $out/src/tools/imagegen/bin/${platformArch}/imagegen
+
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Codex-oriented tool and prompt adapter for pi coding agent";
+    homepage = "https://github.com/IgorWarzocha/howaboua-pi-stuff/tree/main/packages/pi-codex-conversion";
+    license = lib.licenses.mit;
+    platforms = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+  };
+}
